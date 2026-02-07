7473421dc04377fc003bd8190d2e226929d005e7
Bartek Puzio <brtkpuz@narval2.narval.calcul.quebec>
2026-02-07 12:56:52 -0500

    Polishing script initial

---
+++ scripts/D_Polishing.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#apptainer pull docker://quay.io/biocontainers/medaka:2.2.0--py312ha65e1bd_0
+
+set -euo pipefail
+
+ACC=SRR32410565
+ASM="flye_${ACC}/assembly.fasta"
+READS="fastq/${ACC}.nanofilt.q10.l1000.fastq.gz"
+OUTDIR="medaka_${ACC}"
+
+mkdir -p "$OUTDIR"
+
+apptainer exec medaka_2.2.0--py312ha65e1bd_0.sif medaka_consensus \
+  -i "$READS" \
+  -d "$ASM" \
+  -o "$OUTDIR" \
+  -t 8
+
+echo "Polished assembly: ${OUTDIR}/consensus.fasta"
