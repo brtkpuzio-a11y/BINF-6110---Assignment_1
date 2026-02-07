fb11cba6a009d47da31f9941310b5f44985fc5c4
Bartek Puzio <brtkpuz@narval1.narval.calcul.quebec>
2026-02-06 21:49:23 -0500

    Assembly script

---
+++ scripts/Assembly_Flye.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#load module apptainer
+#apptainer pull docker://quay.io/biocontainers/flye:2.9.6--py311h2de2dd3_0
+
+set -euo pipefail
+
+ACC=SRR32410565
+READS="fastq/${ACC}.nanofilt.q10.l1000.fastq.gz"
+OUTDIR="flye_${ACC}"
+
+apptainer exec flye_2.9.6--py311h2de2dd3_0.sif flye --nano-hq "$READS" --genome-size 5m --out-dir "$OUTDIR" --threads 8
+echo "Assembly: ${OUTDIR}/assembly.fasta"
