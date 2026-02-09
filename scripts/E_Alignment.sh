0a5c12f25d7474271c838a2424816fc7994e2df3
Bartek Puzio <brtkpuzio@gmail.com>
2026-02-08 23:34:08 -0500

    Update E_Alignment.sh for assembly alignment

--- scripts/E_Alignment.sh
+++ scripts/E_Alignment.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#module load StdEnv2023
+#module load StdEnv/2023
 #module load minimap2
 #module load samtools
-#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/507/875/GCF_00250787>
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/507/875/GCF_002507875.2_ASM250787v2/GCF_002507875.2_ASM250787v2_genomic.fna.gz
 #gunzip GCF_002507875.2_ASM250787v2_genomic.fna.gz
 
 set -euo pipefail
@@ -10,19 +10,19 @@ set -euo pipefail
 ACC=SRR32410565
 
 # refined reads
-READS=medaka_SRR32410565/consensus.fasta
-REF=GCF_002507875.2_ASM250787v2_genomic.fna
+ASM="medaka_${ACC}/consensus.fasta"
+REF="GCF_002507875.2_ASM250787v2_genomic.fna"
 OUTDIR="mapping_${ACC}"
 
 mkdir -p "$OUTDIR"
 
-# Align ONT reads to reference
-minimap2 -t 8 -ax map-ont "${REF}" "$READS" \
+# Align assembly to reference using the asm5 preset, output BAM, then sort + index
+minimap2 -t 8 -ax asm5 "${REF}" "$ASM" \
 | samtools view -b - \
-| samtools sort -o "${OUTDIR}/${ACC}.ref.sorted.bam" -
+| samtools sort -o "${OUTDIR}/${ACC}.asm_vs_ref.sorted.bam" -
 
-samtools index "${OUTDIR}/${ACC}.ref.sorted.bam"
+samtools index "${OUTDIR}/${ACC}.asm_vs_ref.sorted.bam"
 
 echo "Done."
-echo "BAM: ${OUTDIR}/${ACC}.ref.sorted.bam"
-echo "BAI: ${OUTDIR}/${ACC}.ref.sorted.bam.bai"
+echo "BAM: ${OUTDIR}/${ACC}.asm_vs_ref.sorted.bam"
+echo "BAI: ${OUTDIR}/${ACC}.asm_vs_ref.sorted.bam.bai"
